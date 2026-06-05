457a77524a9f407f4c3e506a6ff35a7feae221fd
Tanmay <github2801@gmail.com>
2026-06-05 09:33:35 +0530

    palindrome Number

---
+++ DSA/Math/Palindrome.java
@@ -0,0 +1,36 @@
+package DSA.Math;
+/*
+Problem:
+Palindrome Number
+
+Link:
+https://leetcode.com/problems/palindrome-number/description/?envType=problem-list-v2&envId=do1uosyi
+
+Approach:
+Reverse the number using division and modulo operations and check if it is same as original number.
+
+Time Complexity:
+O(n)
+
+Space Complexity:
+O(n)
+*/
+
+public class Palindrome {
+    public static void main(String[] args) {
+        int num = 121;
+        int copy = num;
+        int reverse = 0;
+        while (num > 0) {
+            int lastDigit = num % 10;
+            reverse = reverse * 10 + lastDigit;
+            num = num / 10;
+        }
+        if (copy == reverse) {
+            System.out.println(true);
+        } else {
+            System.out.println(false);
+        }
+    }
+
+}
